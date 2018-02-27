8694fe760ef28167c3da02c2100b6d940c8b63d1
VELKUMAR <velkumar1200@gmail.com>
2018-02-27 15:16:50 +0530

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,10 +1,10 @@
-﻿$From = "velkumar.l@thestreet.com"
-$To = "velkumar.l@thestreet.com"
+﻿$From = "From Address"
+$To = "To Address"
 $Cc = "gokulrajkumar.r@thestreet.com"
 $Subject = "Email Subject"
 $Body = "Insert body text here"
-$SMTPServer = "bxsmtppro"
-$SMTPPort = "25"
+$SMTPServer = "SMTP Server"
+$SMTPPort = "Port number"
 Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject `
 -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl `
 -Credential (Get-Credential)
