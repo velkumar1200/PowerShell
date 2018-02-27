184d366452567214cef7e112030485096e5089a7
VELKUMAR <velkumar1200@gmail.com>
2018-02-27 15:12:01 +0530

    Delete Download file.ps1

--- Download file.ps1
+++
@@ -1,3 +0,0 @@
-﻿ $Path = "P:\temp\company_list.xlsx" 
- $source = Start-BitsTransfer -Source "http://www.londonstockexchange.com/statistics/companies-and-issuers/companies-defined-by-mifir-identifiers-list-on-lse.xlsx" -Destination $Path
-Write-Host "File Downloaded in $path"
